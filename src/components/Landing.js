1d72b8019f2991fb680363c8d359367ae8906839
titov-andrei <titov_andrei@mail.ru>
2022-01-26 20:40:19 +0300

    Update Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,10 +3,22 @@ import React from "react";
 class Landing extends React.Component {
   render() {
     return (
-      <div>
-        <div>
-          <div>Выбери ресторан</div>
+      <div className="restaurant_select">
+        <div className="restaurant_select_top">
+          <div className="restaurant_select_top-header">Выбери ресторан</div>
+          <div className="arrow_picker">
+            <div className="arrow_picker-up"></div>
+            <div className="arrow_picker-down"></div>
+          </div>
         </div>
+        <div className="restaurant_select_bottom">
+          <ul>
+            <li>rest 1</li>
+            <li>rest 2</li>
+            <li>rest 3</li>
+          </ul>
+        </div>
+        <button>Перейти в ресторан</button>
       </div>
     );
   }
