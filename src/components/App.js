c052307daf1d627699b110137d66ee69139452a8
titov-andrei <titov_andrei@mail.ru>
2022-01-29 14:10:41 +0300

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="burger-paradise">
+        <div className="menu">
+          <Header />
+        </div>
+        <Order />
+        <MenuAdmin />
+      </div>
+    );
+  }
+}
