78782a078ae127aa865f3534c3a31bf1ef83382f
titov-andrei <titov_andrei@mail.ru>
2022-01-29 18:11:21 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,16 @@ class Header extends React.Component {
               <div className="header-rating_tag">Рейтинг: </div>
               <div className="header-rating_icon">+++</div>
             </div>
+
+            <div className="header-divider"></div>
+            <h1 className="font-effect-fire-animation">Hot Burgers</h1>
+            <h3>
+              <span>
+                Быстрая доставка горячих
+                <span className="sub-header">#бургеров</span>
+              </span>
+            </h3>
+
           </div>
         </div>
       </header>
