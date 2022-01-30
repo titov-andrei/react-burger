ca64a0bac9de66c6fe7e182e74fedb98ceade3ac
titov-andrei <titov_andrei@mail.ru>
2022-01-30 20:13:10 +0300

    Create Router.js

---
+++ src/components/Router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        
+      </Switch>
+    </BrowserRouter>
+  )
+}
