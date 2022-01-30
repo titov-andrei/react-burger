7e3d6c81cb1abb2009484424227e474663728229
titov-andrei <titov_andrei@mail.ru>
2022-01-30 20:38:42 +0300

    Update Router.js

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Landing from './Landing';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
-        
+        <Route exact path='/' component={Landing} />
       </Switch>
     </BrowserRouter>
   )
 }
+
+export default Router;
