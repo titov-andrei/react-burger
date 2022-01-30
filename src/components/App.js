432a8285ce15a33fb13e33d7b0d81d021c73ad37
titov-andrei <titov_andrei@mail.ru>
2022-01-30 13:49:00 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     return (
       <div className="burger-paradise">
         <div className="menu">
-          <Header />
+          <Header title="Very Hot Burger"/>
         </div>
         <Order />
         <MenuAdmin />
