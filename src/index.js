b723ae54508ffd1861c2e8d1a4ccdd91d64b44b6
titov-andrei <titov_andrei@mail.ru>
2022-01-31 08:42:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 // import Landing from "./components/Landing";
-import App from "./components/App";
+import Router from "./components/Router";
 import "./css/style.css";
 
-render(<App />, document.querySelector("#root"));
+render(<Router />, document.querySelector("#root"));
