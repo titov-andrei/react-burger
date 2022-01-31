8528c12c708bce2d2b57711dae6d5e13bdf747bc
titov-andrei <titov_andrei@mail.ru>
2022-01-31 12:17:06 +0300

    Update AddBurgerForm.js

--- src/components/AddBurgerForm.js
+++ src/components/AddBurgerForm.js
@@ -3,9 +3,13 @@ import React from "react";
 class AddBurgerForm extends React.Component {
   render() {
     return (
-    <form>
-
-    </form>
+      <form>
+        <input name="name" type="text" placeholder="Name" autoComplete="off" />
+        <input name="price" type="text" placeholder="Name" autoComplete="off" />
+        <input name="status" type="text" placeholder="Name" autoComplete="off" />
+        <input name="desc" type="text" placeholder="Name" autoComplete="off" />
+        <input name="image" type="text" placeholder="Name" autoComplete="off" />
+      </form>
     );
   }
 }
