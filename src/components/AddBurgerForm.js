8bcdc204a595d9aef71e1356e2323271040486c5
titov-andrei <titov_andrei@mail.ru>
2022-01-31 17:02:25 +0300

    Update AddBurgerForm.js

--- src/components/AddBurgerForm.js
+++ src/components/AddBurgerForm.js
@@ -15,7 +15,7 @@ class AddBurgerForm extends React.Component {
       desc: this.descRef.current.value,
       image: this.imageRef.current.value,
     };
-    console.log(burger);
+    this.props.addBurger(burger);
   };
   render() {
     return (
