691ee75145904a7bb49c218d0851bbaa43d65320
titov-andrei <titov_andrei@mail.ru>
2022-02-01 16:37:15 +0300

    Update Burger.js

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -5,7 +5,10 @@ class Burger extends React.Component {
     return (
       <li className="menu-burger">
         <div className="image-container">
-          <img src={this.props.details.image} alt="img"/>
+          <img src={this.props.details.image} alt="img" />
+        </div>
+        <div className="burger-details">
+          <h3 className="burger-name">{this.props.details.name}</h3>
         </div>
       </li>
     );
