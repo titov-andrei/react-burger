7aab118aa0fe91ae8f494245136e62f3e2e6601e
titov-andrei <titov_andrei@mail.ru>
2022-02-01 20:40:24 +0300

    Update Burger.js

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -3,7 +3,7 @@ import React from "react";
 class Burger extends React.Component {
   render() {
     const { image, name, price, desc, status } = this.props.details;
-    const isAvailable = status === 'available';
+    const isAvailable = status === "available";
     return (
       <li className="menu-burger">
         <div className="image-container">
@@ -15,7 +15,9 @@ class Burger extends React.Component {
             <span className="price">{price} ₽</span>
           </h3>
           <p>{desc}</p>
-          <button className="buttonOrder">Заказать</button>
+          <button className="buttonOrder" disabled={!isAvailable}>
+            {isAvailable ? 'Заказать' : 'Временно нет'}
+          </button>
         </div>
       </li>
     );
