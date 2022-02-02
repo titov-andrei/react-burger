c011dd09b72b8cfcf461f0472d3e1c5afbd4be4a
titov-andrei <titov_andrei@mail.ru>
2022-02-02 13:39:31 +0300

    Update Order.js

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
 
 class Order extends React.Component {
   render() {
+    const orderIds = Object.keys(this.props.order);
     return (
       <div className="order-wrap">
         <h2>Ваш Заказ</h2>
+        <ul className="order">
+          {orderIds.map((key) => {
+            return <li>{key}</li>;
+          })}
+        </ul>
       </div>
     );
   }
