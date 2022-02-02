4590c349459eb91754a4aaeea52a1af1e1d89e73
titov-andrei <titov_andrei@mail.ru>
2022-02-02 13:54:32 +0300

    Update Order.js

--- src/components/Order.js
+++ src/components/Order.js
@@ -3,6 +3,9 @@ import React from "react";
 class Order extends React.Component {
   render() {
     const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const burger = this.props.burgers[key];
+    });
     return (
       <div className="order-wrap">
         <h2>Ваш Заказ</h2>
