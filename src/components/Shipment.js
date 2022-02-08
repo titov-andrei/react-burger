90dd0462da21e2a6bdc23163091ad10839d61605
titov-andrei <titov_andrei@mail.ru>
2022-02-08 13:03:32 +0300

    Create Shipment.js

---
+++ src/components/Shipment.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Shipment extends Component {
+  render() {
+    return (
+      <div className="total">
+        <div className="total_wrap">
+          <div className="total_wrap-final">Итого: {total} ₽</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Shipment;
