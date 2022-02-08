39ddba486141f6aa01409c5ddda2b5600d612e65
titov-andrei <titov_andrei@mail.ru>
2022-02-08 14:55:14 +0300

    Update Shipment.js

--- src/components/Shipment.js
+++ src/components/Shipment.js
@@ -3,9 +3,24 @@ import React, { Component } from "react";
 class Shipment extends Component {
   render() {
     const { total } = this.props;
+    const shipping = total > 0 && total < 500 ? 350 : 99;
+    const shippingNeon =
+      shipping === 99 ? (
+        <span className="font-effect-neon total_wrap-cheap">{shipping} ₽</span>
+      ) : (
+        <span>{shipping} ₽</span>
+      );
     return (
       <div className="total">
         <div className="total_wrap">
+          <div>
+            <div>Доставка: {total > 0 ? shippingNeon : null}</div>
+            <div className="total-wrap-free">
+              {total < 500
+                ? `Закажите ещё на ${500 - total} ₽ для доставки за 99 ₽`
+                : null}
+            </div>
+          </div>
           <div className="total_wrap-final">Итого: {total} ₽</div>
         </div>
       </div>
