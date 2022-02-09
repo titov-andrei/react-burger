e0a04f42d765b5ff9e0345cbde5b34c08e56dd90
titov-andrei <titov_andrei@mail.ru>
2022-02-09 13:01:46 +0300

    Update base.js

--- src/base.js
+++ src/base.js
@@ -2,5 +2,7 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  
+  apiKey: "AIzaSyCT5QlnziLmoRJQ-2TGVb4e-M7yc0eF2gc",
+  authDomain: "very-hot-burgers-768f4.firebaseapp.com",
+  databaseURL: "https://very-hot-burgers-768f4-default-rtdb.firebaseio.com",
 })
