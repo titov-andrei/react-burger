0b415cf9062422011fdcc993e17b30a981706357
titov-andrei <titov_andrei@mail.ru>
2022-02-12 10:39:34 +0300

    Update Order.js

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Shipment from './Shipment';
+import Shipment from "./Shipment";
 
 class Order extends React.Component {
   renderOrder = (key) => {
     const burger = this.props.burgers[key];
     const count = this.props.order[key];
     const isAvailable = burger && burger.status === "available";
+    if (!burger) return null;
     if (!isAvailable) {
       return (
         <li className="unavailable" key={key}>
