a836a1b606b1ee4707f5be1c0ecff8fb60767c7f
titov-andrei <titov_andrei@mail.ru>
2022-02-12 10:39:29 +0300

    Create EditBurgerForm.js

---
+++ src/components/EditBurgerForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class EditBurgerForm extends React.Component {
+  render() {
+    return (
+      <div className="burger-edit">
+        <input />
+      </div>
+    );
+  }
+}
+
+export default EditBurgerForm;
