b1e2f3f29eb2dd6c0f5d38a5c4bd8ef27d59acd8
titov-andrei <titov_andrei@mail.ru>
2022-02-12 13:20:56 +0300

    Update EditBurgerForm.js

--- src/components/EditBurgerForm.js
+++ src/components/EditBurgerForm.js
@@ -4,7 +4,14 @@ class EditBurgerForm extends React.Component {
   render() {
     return (
       <div className="burger-edit">
-        <input />
+        <input name='name' type='text' />
+        <input name='price' type='text' />
+        <select name='status'>
+          <option value='available'>Доступно!</option>
+          <option value='unavailable'>Не доступно!</option>
+        </select>
+        <input name='desc' type='text' />
+        <input name='image' type='text' />
       </div>
     );
   }
