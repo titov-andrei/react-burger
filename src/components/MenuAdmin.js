59bda490d853d2be10edab7dc6cad057aa5ae657
titov-andrei <titov_andrei@mail.ru>
2022-02-12 11:44:03 +0300

    Update MenuAdmin.js

--- src/components/MenuAdmin.js
+++ src/components/MenuAdmin.js
@@ -1,13 +1,19 @@
 import React from "react";
-import AddBurgerForm from './AddBurgerForm';
+import AddBurgerForm from "./AddBurgerForm";
+import EditBurgerForm from "./EditBurgerForm";
 
 class MenuAdmin extends React.Component {
   render() {
     return (
       <div className="menu-admin">
         <h2>Управление Меню</h2>
+        {Object.keys(this.props.burgers).map((key) => {
+          return <EditBurgerForm />;
+        })}
         <AddBurgerForm addBurger={this.props.addBurger} />
-        <button onClick={this.props.loadSampleBurgers}>Загрузить бургеры</button>
+        <button onClick={this.props.loadSampleBurgers}>
+          Загрузить бургеры
+        </button>
       </div>
     );
   }
