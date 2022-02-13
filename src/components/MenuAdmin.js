8c00b08589c4572f33d4c3e5dd110781ca9cadff
titov-andrei <titov_andrei@mail.ru>
2022-02-13 13:15:30 +0300

    Update MenuAdmin.js

--- src/components/MenuAdmin.js
+++ src/components/MenuAdmin.js
@@ -8,7 +8,7 @@ class MenuAdmin extends React.Component {
       <div className="menu-admin">
         <h2>Управление Меню</h2>
         {Object.keys(this.props.burgers).map((key) => {
-          return <EditBurgerForm burger={this.props.burgers[key]} />;
+          return <EditBurgerForm key={key} burger={this.props.burgers[key]} />;
         })}
         <AddBurgerForm addBurger={this.props.addBurger} />
         <button onClick={this.props.loadSampleBurgers}>
