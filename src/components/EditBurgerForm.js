eb84e293dfdaed9a474ba5521bd9d854e5b20356
titov-andrei <titov_andrei@mail.ru>
2022-02-14 14:36:39 +0300

    Update EditBurgerForm.js

--- src/components/EditBurgerForm.js
+++ src/components/EditBurgerForm.js
@@ -5,14 +5,26 @@ class EditBurgerForm extends React.Component {
     const updatedBurger = {
       ...this.props.burger,
       [event.currentTarget.name]: event.currentTarget.value,
-    }
-  }
+    };
+    this.props.updateBurger(this.props.index, updatedBurger);
+  };
   render() {
     return (
       <div className="burger-edit">
-        <input onChange={this.handleChange} name="name" type="text" value={this.props.burger.name} />
-        <input onChange={this.handleChange} name="price" type="text" value={this.props.burger.price} />
-        <select onChange={this.handleChange}
+        <input
+          onChange={this.handleChange}
+          name="name"
+          type="text"
+          value={this.props.burger.name}
+        />
+        <input
+          onChange={this.handleChange}
+          name="price"
+          type="text"
+          value={this.props.burger.price}
+        />
+        <select
+          onChange={this.handleChange}
           className="status"
           name="status"
           value={this.props.burger.status}
@@ -20,8 +32,17 @@ class EditBurgerForm extends React.Component {
           <option value="available">Доступно!</option>
           <option value="unavailable">Не доступно!</option>
         </select>
-        <textarea onChange={this.handleChange} name="desc" value={this.props.burger.desc} />
-        <input onChange={this.handleChange} name="image" type="text" value={this.props.burger.image} />
+        <textarea
+          onChange={this.handleChange}
+          name="desc"
+          value={this.props.burger.desc}
+        />
+        <input
+          onChange={this.handleChange}
+          name="image"
+          type="text"
+          value={this.props.burger.image}
+        />
       </div>
     );
   }
